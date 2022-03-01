ee7734382248c992af9861c35423f6c36ce87730
fanjiaming <954574569@qq.com>
2022-03-01 14:51:56 +0800

    feat(canteen-order):测试启动

--- apps/canteen-order/src/main.ts
+++ apps/canteen-order/src/main.ts
@@ -15,10 +15,10 @@ async function bootstrap() {
 
   app.enableCors();
   app.use(
-    // rateLimit({
-    //   windowMs: 15 * 60 * 1000, // 15 minutes
-    //   max: 100, // limit each IP to 100 requests per windowMs
-    // }),
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
     helmet(),
     json({ limit: '1mb' }),
     urlencoded({ extended: true, limit: '1mb' }),
